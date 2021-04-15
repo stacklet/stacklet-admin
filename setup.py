4003963d48e997ba7f914234a9cf2f718cca31b9
thisisshi <theli@outlook.com>
2021-04-15 00:27:03 -0700

    move to poetry to install cli

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="stacklet",
-    version="0.0.1",
-    install_requires=["Click"],
-    entry_points={"console_scripts": ["stacklet = cli.cli:cli"]},
-)
