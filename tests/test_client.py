83180c1b3db4cff08ce7da359229dca9d942da71
Sonny <theli@outlook.com>
2022-05-19 10:17:13 -0700

    fix: import commands so platform client has them when instantiated (#39)
    * fix: import commands so platform client has them when instantiated
    
    * lint
    
    * patch fix

---
+++ tests/test_client.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+from stacklet.client.platform.client import platform_client
+
+
+@patch("stacklet.client.platform.client.get_token")
+@patch("stacklet.client.platform.client.StackletContext")
+@patch("stacklet.client.platform.client.Path")
+def test_client_loaded_commands(
+    patched_path, patched_stacklet_context, patched_get_token
+):
+    client = platform_client()
+    assert hasattr(client, "list_bindings")
+    assert hasattr(client, "list_accounts")
+    assert hasattr(client, "list_repository")
+    assert hasattr(client, "list_policies")
