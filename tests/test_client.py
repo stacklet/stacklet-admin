54e78ccbbe1ca401bcbd55032f1b50285ca05444
thisisshi <theli@outlook.com>
2022-05-18 19:28:18 -0700

    lint

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,7 +3,7 @@ from stacklet.client.platform.client import platform_client
 
 def test_client_loaded_commands():
     client = platform_client()
-    assert hasattr(client, 'list_bindings')
-    assert hasattr(client, 'list_accounts')
-    assert hasattr(client, 'list_repository')
-    assert hasattr(client, 'list_policies')
+    assert hasattr(client, "list_bindings")
+    assert hasattr(client, "list_accounts")
+    assert hasattr(client, "list_repository")
+    assert hasattr(client, "list_policies")
