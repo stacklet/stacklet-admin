8a56223e27c06e48524f214c447bbe79cc39f706
thisisshi <theli@outlook.com>
2022-05-19 10:01:02 -0700

    patch fix

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,14 @@
+from unittest.mock import patch
+
 from stacklet.client.platform.client import platform_client
 
 
-def test_client_loaded_commands():
+@patch("stacklet.client.platform.client.get_token")
+@patch("stacklet.client.platform.client.StackletContext")
+@patch("stacklet.client.platform.client.Path")
+def test_client_loaded_commands(
+    patched_path, patched_stacklet_context, patched_get_token
+):
     client = platform_client()
     assert hasattr(client, "list_bindings")
     assert hasattr(client, "list_accounts")
